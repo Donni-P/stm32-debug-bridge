133622d98c5e2d006bc24823aec5dbbf9d0ee7ca
Donni-P <dputya@mail.ru>
2023-09-25 00:23:11 +0300

    попытка пофиксить ошибку BUSY

--- src/main.cpp
+++ src/main.cpp
@@ -54,10 +54,171 @@ extern "C" void __terminate() {
         ;
 }
 
+template<typename uint_T,int amount_regs>
+class Hal{
+public:
+    static void clear(){
+        for(int i = 0; i < amount_regs; i++)
+            mem[i] = 0x0;
+    }
+    static void rx(uint_T * mm){
+        clear();
+        for(int i = 0; i < amount_regs; i++){
+            mem[i] = mm[i];
+        }
+    }
+    static uint_T tx(){
+        return mem;
+    }
+    static inline uint_T mem[amount_regs] = {0x0};
+};
+template<typename uint_T, int amount_regs, class HAL1, class HAL2>
+class SlaveI2C {
+public:
+    enum size{
+        bits_8 = 1,
+        bits_16,
+        bits_32 = 4
+    };
+    enum state{
+        stay,//нет передачи, ждется START
+        busy,//состояние, в котором после каждого байта проверяется есть ли START или STOP
+        receive,//запись байта в регистр
+        transmit,//передача байта в мастер
+
+    };
+    void reset(){
+        for(int i = 0; i < amount_regs; i++)
+            mem[i] = 0x0;
+        ind_reg = 0;
+    }
+    void send_toHAL(){
+        if(!dualf)
+            HAL1::rx(mem);
+        else HAL2::rx(mem);
+        reset();
+    }
+    void rx_fromHAL(){
+        uint_T * mm;
+        if(!dualf)
+            mm = HAL1::tx();
+        else mm = HAL2::tx();
+        for(int i = 0; i < amount_regs; i++){
+            mem[i] = mm[i];
+        }
+    }
+    void read_fromMaster(size nbytes){
+        if(ind_reg < (amount_regs * nbytes)){
+            mem[ind_reg / nbytes] |= (i2c_crl->DR << (8 * (ind_reg % nbytes)));
+            ind_reg++;
+        }
+    }
+    void write_toMaster(size nbytes){
+        uint8_t byte;
+        if(ind_reg < (amount_regs * nbytes)){
+            byte = mem[ind_reg / nbytes] >> (8 * (ind_reg % nbytes));
+            i2c_crl->DR = byte; 
+        }
+    }
+    void set_modeHAL() {
+        if((i2c_crl->SR2 & I2C_SR2_TRA_Msk) == 0){
+            slv_state = receive;
+            tra = false;
+        }else {
+            slv_state = transmit;
+            tra = true;
+        }
+        if((i2c_crl->SR2 & I2C_SR2_DUALF_Msk) == 0)
+            dualf = false;
+        else dualf = true;
+    }
+    bool addr_rx(){
+        bool res = false;
+        if((i2c_crl->SR1 & I2C_SR1_ADDR_Msk) != 0){
+            i2c_crl->SR1 = i2c_crl->SR1;
+            i2c_crl->SR2 = i2c_crl->SR2;
+            set_modeHAL();
+            res = true;
+        }
+        return res;
+    }
+    void check(){ //точка входа
+        switch(slv_state){
+            case stay:
+                addr_rx();
+                break;
+            case busy:
+                if(addr_rx()){
+                    if(!tra) send_toHAL();
+                }else if((i2c_crl->SR1 & I2C_SR1_STOPF_Msk) != 0){
+                    i2c_crl->SR1 = i2c_crl->SR1;
+                    i2c_crl->CR1 = i2c_crl->CR1;
+                    send_toHAL();
+                    slv_state = stay;
+                }else if((i2c_crl->SR1 & I2C_SR1_AF_Msk) != 0){
+                    i2c_crl->SR1 = i2c_crl->SR1 & ~I2C_SR1_AF_Msk;
+                    reset();
+                }else set_modeHAL();
+                break;
+            case receive:
+                //получение
+                if((i2c_crl->SR1 & I2C_SR1_RXNE_Msk) != 0){
+                    switch(slv_size){
+                        case bits_8:
+                            read_fromMaster(bits_8);
+                            break;
+                        case bits_16:
+                            read_fromMaster(bits_16);
+                            break;
+                        case bits_32:
+                            read_fromMaster(bits_32);
+                            break;
+                            
+                    }
+                    slv_state = busy;
+                }
+                break;
+            case transmit:
+                //передача
+                if((i2c_crl->SR1 & I2C_SR1_TXE_Msk) != 0){
+                    switch(slv_size){
+                        case bits_8:
+                            write_toMaster(bits_8);
+                            break;
+                        case bits_16:
+                            write_toMaster(bits_16);
+                            break;
+                        case bits_32:
+                            write_toMaster(bits_32);
+                            break;
+                            
+                    }
+                    slv_state = busy;
+                }
+                break;
+        }
+    }
+    SlaveI2C(I2C_TypeDef *i2c):slv_state(stay),ind_reg(0),i2c_crl(i2c){
+        slv_size = static_cast<size>(sizeof(uint_T));
+    }
+
+private:
+    state slv_state;
+    size slv_size;
+    int ind_reg;
+    uint_T mem[amount_regs] = {0x0};
+    I2C_TypeDef * const i2c_crl;
+    bool tra = false;
+    bool dualf = false;
+};
+
+
 int main() {
     clock::init();
     SystemCoreClockUpdate();
     PortsInit();
+
+    SlaveI2C<uint32_t, 4, Hal<uint32_t,4>,Hal<uint32_t,4>> x(I2C2);
 
     uint32_t lastDmaRxLen = 0;
     uint32_t lastDmaTxLen = 0;
@@ -78,17 +239,39 @@ int main() {
 
     usb::init();
     __enable_irq();
-
+    RCC->APB1ENR = RCC->APB1ENR | RCC_APB1ENR_I2C1EN | RCC_APB1ENR_I2C2EN;
     RCC->APB2ENR = (RCC->APB2ENR & ~RCC_APB2ENR_IOPBEN_Msk) | RCC_APB2ENR_IOPBEN;
     //I2C1
-    GPIOB->CRL = GPIOB->CRL | (0xff << 24);//8 единиц в старших битах
-    GPIOB->ODR = GPIOB->ODR | (0x3 << 6);// 11 в 7 и 6 битах ODR
+    GPIOB->CRL = (GPIOB->CRL & ~(0xff << 24)) | (0x77 << 24);//general open-drain
+    GPIOB->ODR = (GPIOB->ODR & ~(0x3 << 6)) | (0x3 << 6);// 11 в 7 и 6 битах ODR
+    while((GPIOB->IDR & (0x3 << 6)) == 0){}
+    GPIOB->ODR = GPIOB->ODR & ~(0x1 << 6);
+    while((GPIOB->IDR & (0x1 << 6)) != 0){}
+    GPIOB->ODR = GPIOB->ODR | (0x1 << 6);
+    while((GPIOB->IDR & (0x1 << 6)) == 0){}
+    GPIOB->ODR = GPIOB->ODR & ~(0x1 << 7);
+    while((GPIOB->IDR & (0x1 << 7)) != 0){}
+    GPIOB->ODR = GPIOB->ODR | (0x1 << 7);
+    while((GPIOB->IDR & (0x1 << 7)) == 0){}
     //I2C2
-    GPIOB->CRH = GPIOB->CRH | (0xff << 8);//8 единиц в cnf & mode 10/11
-    GPIOB->ODR = GPIOB->ODR | (0x3 << 10);// 11 в 7 и 6 битах ODR
+    GPIOB->CRH = (GPIOB->CRH & ~(0xff << 8)) | (0x77 << 8);//general open-drain
+    GPIOB->ODR = (GPIOB->ODR & ~(0x3 << 10)) | (0x3 << 10);// 11 в 7 и 6 битах ODR
+    while((GPIOB->IDR & (0x3 << 10)) == 0){}
+    GPIOB->ODR = GPIOB->ODR & ~(0x1 << 10);
+    while((GPIOB->IDR & (0x1 << 10)) != 0){}
+    GPIOB->ODR = GPIOB->ODR | (0x1 << 10);
+    while((GPIOB->IDR & (0x1 << 10)) == 0){}
+    GPIOB->ODR = GPIOB->ODR & ~(0x1 << 11);
+    while((GPIOB->IDR & (0x1 << 11)) != 0){}
+    GPIOB->ODR = GPIOB->ODR | (0x1 << 11);
+    while((GPIOB->IDR & (0x1 << 11)) == 0){}
     //I2C1
-    RCC->APB1ENR = RCC->APB1ENR | RCC_APB1ENR_I2C1EN | RCC_APB1ENR_I2C2EN;
+    GPIOB->CRL = (GPIOB->CRL & ~(0xff << 24)) | (0xff << 24);//alternate open-drain
+    //I2C2
+    GPIOB->CRH = (GPIOB->CRH & ~(0xff << 8)) | (0xff << 8);//alternate open-drain
+    //I2C1
     I2C1->CR1 = I2C1->CR1 | I2C_CR1_SWRST;
+    while((I2C1->SR2 & I2C_SR2_BUSY_Msk) != 0){}
     I2C1->CR1 = I2C1->CR1 & ~I2C_CR1_SWRST_Msk;
     //I2C1->OAR1 = (0x11 << 1);
     I2C1->CR2 = 0x24; // 36 MHz
@@ -97,6 +280,7 @@ int main() {
     I2C1->CR1 = I2C1->CR1 | I2C_CR1_ACK | I2C_CR1_PE;
     //I2C2
     I2C2->CR1 = I2C2->CR1 | I2C_CR1_SWRST;
+    while((I2C2->SR2 & I2C_SR2_BUSY_Msk) != 0){}
     I2C2->CR1 = I2C2->CR1 & ~I2C_CR1_SWRST_Msk;
     I2C2->OAR1 = (0x33 << 1);
     I2C2->OAR2 = (0x42 << 1) | 1;
